73bc1b5be3c14cca6191c41a138ac7a19217482c
Angel Rojas <angelnest1602@live.com.mx>
2023-11-09 01:20:10 -0600

    Primer Commit

---
+++ assets/js/custom.js
@@ -0,0 +1,5 @@
+// Obtén el número de reseñas (esto podría provenir de una API, base de datos, etc.)
+var numeroResenas = obtenerNumeroDeResenas(); // Esta función debería obtener el número de reseñas de alguna fuente de datos
+
+// Actualiza el contenido del elemento con el ID 'numero-resenas' con el número de reseñas
+document.getElementById("numero-resenas").textContent = numeroResenas;
